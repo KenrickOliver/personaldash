c15a846def83c31b9e24b34e681b95d22de7d71a
KenrickOliver <kenrick.oliver@gmail.com>
2021-07-14 20:57:14 -0400

    finished fetch weather feature

--- script.js
+++ script.js
@@ -9,6 +9,53 @@ saveBtn.addEventListener('click', () => {
     console.log('click')
 })
 
+//Fetch weather data
+navigator.geolocation.getCurrentPosition(position => {
+
+    let lon = position.coords.longitude;
+    let lat = position.coords.latitude;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=769398ad3f3991bbc1a073eb37da612a`)
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Weather data not available")
+        }
+        return res.json()
+    })
+    .then(data => {
+        console.log(data)
+        const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        document.getElementById("weather").innerHTML = `
+        <h3>Weather in: ${data.name}</h3>
+        <p class="weather-temp">${Math.round(data.main.temp)} ºC</p>
+        <img
+          id="weather-icon"
+          class="weather-icon"
+          src=${iconUrl}
+          alt="weather icon"
+        />
+        <div class="weather-info>
+            <p class="weather-description">${data.weather[0].description}</p>
+            <p class="weather-humidity">${data.main.humidity} %</p>
+            <p class="weather-wind">${data.wind.speed} km/h</p>
+        </div>
+        `
+    })
+    .catch(err => console.error(err))
+    
+});
+
+/*
+coords: GeolocationCoordinates
+accuracy: 1960
+altitude: null
+altitudeAccuracy: null
+heading: null
+latitude: 43.58144
+longitude: -79.6229632
+speed: null
+*/
+
 // Function to getquote
 function getQuote() {
     
