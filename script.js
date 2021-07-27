83fc5de4d0494ea89fe5e87ec13cb20baa417c8c
KenrickOliver <kenrick.oliver@gmail.com>
2021-07-26 23:45:11 -0400

    added timer and cycles tracker with 1 min time for testing

--- script.js
+++ script.js
@@ -90,7 +90,101 @@ function setTime() {
 
 setInterval(setTime, 1000); //Update the time every second(1000 milliseconds)
 
-// ***** NOTES Functionality
+// ***** TIMER Functionality
+let start = document.getElementById('start');
+let stop = document.getElementById('stop');
+let reset = document.getElementById('reset');
+
+let workMinutes = document.getElementById('w-minutes');
+let workSeconds = document.getElementById('w-seconds');
+
+let breakMinutes = document.getElementById('b-minutes');
+let breakSeconds = document.getElementById('b-seconds');
+
+let listCounter = document.querySelector('.counter-list')
+
+//store a reference to a timer variable
+let startTimer;
+
+start.addEventListener('click', function(){
+    if(startTimer === undefined){
+        startTimer = setInterval(timer, 1000)
+    } else {
+        alert("Timer is already running");
+    }
+})
+
+reset.addEventListener('click', function(){
+    workMinutes.innerText = 1;
+    workSeconds.innerText = "00";
+
+    breakMinutes.innerText = 1;
+    breakSeconds.innerText = "00";
+
+    document.getElementById('counter').innerText = 0;
+    listCounter.innerHTML = '';
+    stopInterval()
+    startTimer = undefined;
+})
+
+stop.addEventListener('click', function(){
+    stopInterval()
+    startTimer = undefined;
+})
+
+
+//Start Timer Function
+function timer(){
+    //Work Timer Countdown
+    if(workSeconds.innerText != 0){
+        workSeconds.innerText--;
+    } else if(workMinutes.innerText != 0 && workSeconds.innerText == 0){
+        workSeconds.innerText = 59;
+        workMinutes.innerText--;
+    }
+
+    //Break Timer Countdown
+    if(workMinutes.innerText == 0 && workSeconds.innerText == 0){
+        if(breakSeconds.innerText != 0){
+            breakSeconds.innerText--;
+        } else if(breakMinutes.innerText != 0 && breakSeconds.innerText == 0){
+            breakSeconds.innerText = 59;
+            breakMinutes.innerText--;
+        }
+    }
+
+    //Increment Counter by one if one full cycle is completed
+    if(workMinutes.innerText == 0 && workSeconds.innerText == 0 && breakMinutes.innerText == 0 && breakSeconds.innerText == 0){
+        workMinutes.innerText = 1;
+        workSeconds.innerText = "00";
+
+        breakMinutes.innerText = 1;
+        breakSeconds.innerText = "00";
+
+        document.getElementById('counter').innerText++;
+        addItem()
+    }
+}
+
+//Stop Timer Function
+function stopInterval(){
+    clearInterval(startTimer);
+}
+
+// Tracking Cycles via peaches
+function addItem() {
+    let newImage = document.createElement('img')
+    newImage.setAttribute('class', 'image-item')
+    newImage.setAttribute('alt', 'work peach')
+    newImage.src = './assets/peach.png';
+
+    let liItem = document.createElement('li')
+    liItem.setAttribute('class', 'li-item')
+    liItem.appendChild(newImage)
+    listCounter.appendChild(liItem)
+}
+
+// ***** NOTES Functionality *****
 
 const addBtn = document.getElementById('add')
 const notesContainer = document.querySelector('.notes-container')
